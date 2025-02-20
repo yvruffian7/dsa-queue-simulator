829c8c56905d458d3e80209b77b2881e6c24eb33
bivamshu <bivamshu.khadka86@gmail.com>
2025-02-20 15:24:50 +0545

    queue file

---
+++ src/queue.cpp
@@ -0,0 +1,15 @@
+#include "queue.h"
+#include <iostream>
+
+
+template <typename T> void Queue<T>::enqueue(const T &value) {
+  elements.push_back(value);
+}
+
+template <typename T> bool Queue<T>::empty() { return elements.empty(); }
+
+template <typename T> void Queue<T>::dequeue() {
+  if (!empty()) {
+    elements.erase(elements.begin());
+  }
+}
