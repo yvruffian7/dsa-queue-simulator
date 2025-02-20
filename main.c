9db75bb7e339251746afd9c10c3a0e2ea23f58bc
bivamshu <bivamshu.khadka86@gmail.com>
2025-02-20 15:19:01 +0545

    update c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-#new
+#include "main.h"
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3_ttf/SDL_ttf.h"
+#include "text.h"
+#include <SDL3/SDL_render.h>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
