8be14125e4fc20182472f26c5f1e374449d40fd2
aaditya <adiy7225@gmail.com>
2025-02-27 13:51:21 +0545

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,89 +1,15 @@
-#include "main.h"
-#include "SDL3/SDL_events.h"
-#include "SDL3/SDL_init.h"
-#include "SDL3/SDL_log.h"
-#include "SDL3_ttf/SDL_ttf.h"
-#include "text.h"
-#include <SDL3/SDL_render.h>
-#include <exception>
-#include <memory>
-#include <stdexcept>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "traffic_simulation.h"
 
-namespace GameVariable {
-constexpr int WindowHeight = 720;
-constexpr int WindowLength = 1280;
-constexpr int TextPixel = 30;
+#define FPS 60
+#define FRAME_DELAY (1000 / FPS)
 
-} // namespace GameVariable
-
-App::App()
-    : m_window("Traffic Simulator", GameVariable::WindowLength,
-               GameVariable::WindowHeight) {
-
-  if (TTF_Init() < 0) {
-    throw std::runtime_error(SDL_GetError());
-  }
-
-  m_font = TTF_OpenFont("../assets/fonts/DaddyTimeMonoNerdFontMono-Regular.ttf",
-                        GameVariable::TextPixel);
-  if (!m_font) {
-    throw std::runtime_error(SDL_GetError());
-  }
-
-  m_text = std::make_unique<Text>(m_window.renderer(), m_font);
-  m_text->setText("Traffic Simulator", SDL_Color{255, 255, 255, 255});
-
-  // Initialize other sub system
+void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
+    SDL_Init(SDL_INIT_VIDEO);
+    *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(*renderer, 144, 238, 144, 255); // Light blue
+ 
 }
-
-App::~App() {
-  // Clean up
-}
-
-void App::run() {
-  while (m_running) {
-    process_event();
-    update();
-    render();
-  }
-}
-
-void App::process_event() {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    if (event.type == SDL_EVENT_QUIT) {
-      m_running = false;
-    }
-  }
-}
-
-void App::update() {}
-void App::render() {
-
-  m_window.clear();
-
-  m_text->render(20, 30);
-
-  // Rendering code goes here
-  m_window.present();
-}
-
-int main(int argc, const char *argv[]) {
-
-  (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS));
-
-  TTF_Font *font =
-      TTF_OpenFont("../assets/fonts/DaddyTimeMonoNerdFontMono-Regular.ttf", 10);
-  try {
-    App app;
-    app.run();
-
-  } catch (const std::exception &e) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Error: %s\n", e.what());
-  }
-
-  SDL_Quit();
-  return 0;
-}
-
