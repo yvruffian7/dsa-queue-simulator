dac93b6700b343efd12f23997223daa2b49923c0
aaditya <adiy7225@gmail.com>
2025-02-27 13:57:05 +0545

    Create traffic_simulation.h

---
+++ src/traffic_simulation.h
@@ -0,0 +1,12 @@
+#ifndef TRAFFIC_SIMULATION_H
+#define TRAFFIC_SIMULATION_H
+
+#include <SDL.h>
+#include <stdbool.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define LANE_WIDTH 80
+#define MAX_VEHICLES 10
+#define INTERSECTION_X (WINDOW_WIDTH / 2)
+#define INTERSECTION_Y (WINDOW_HEIGHT / 2)
